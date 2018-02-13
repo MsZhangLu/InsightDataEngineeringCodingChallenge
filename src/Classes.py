994bb08d990e61d89d5d56d08accedd81f61af12
viki1016 <zhan716@usc.edu>
2018-02-13 01:51:04 -0800

    .

---
+++ src/Classes.py
@@ -0,0 +1,76 @@
+import re
+
+class Donation:
+	def __init__(self, CMTE_ID, NAME, ZIP_CODE, TRANSACTION_DT, TRANSACTION_AMT, OTHER_ID):
+		self.CMTE_ID = CMTE_ID
+		self.NAME = NAME
+		self.ZIP_CODE = ZIP_CODE
+		self.TRANSACTION_DT = TRANSACTION_DT
+		self.TRANSACTION_AMT = TRANSACTION_AMT
+		self.OTHER_ID = OTHER_ID
+
+	def isValid(self):
+		if (
+			self.CMTE_ID is None or re.match('^[a-zA-Z0-9\s]*$', self.CMTE_ID) is None or
+			self.NAME is None or re.match('^[a-zA-Z0-9\s.,]*$', self.NAME) is None or
+			self.ZIP_CODE is None or len(self.ZIP_CODE) < 5 or re.match('^[0-9]*$', self.ZIP_CODE) is None or
+			self.TRANSACTION_DT is None or 
+			re.match('^((0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[12]\d{3})$', self.TRANSACTION_DT) is None or
+			self.TRANSACTION_AMT is None or 
+			len(self.OTHER_ID) > 0
+			):
+			return False
+		else:
+			return True
+
+	def getID(self):
+		return self.NAME + self.ZIP_CODE
+
+	def getYear(self):
+		return int(self.TRANSACTION_DT[4:])
+
+	def getRecipient(self):
+		return self.CMTE_ID
+
+	def getZip(self):
+		return self.ZIP_CODE
+
+	def getAmt(self): 
+		return self.TRANSACTION_AMT
+
+
+class Dictionary:
+	def __init__(self):
+		self.dictionary = {}
+
+	def add(self, key, year):
+		if key in self.dictionary:
+			self.dictionary[key][0] += 1
+		else:
+			self.dictionary[key] = (1, year)
+
+	def add_long(self, recipient, zipcode, year, amt):
+		key = (recipient, zipcode, year)
+		if key in self.dictionary:
+			self.dictionary[0] += 1
+			self.dictionary[1] += amt
+		else:
+			self.dictionary[key] = (1, amt) 
+
+	def drop(self, key):
+		self.dictionary.pop(key)
+
+	def check(self, key):
+		return True if key in self.dictionary else False
+
+	def get(self, key):
+		return self.dictionary[key]
+
+class Output:
+	def __init__(self, recipient, zipcode, year, percentile, amt, donors):
+		self.recipient = recipient
+		self.zipcode = zipcode
+		self.year = year
+		self.percentile = percentile
+		self.amt = amt
+		self.donors = donors
--- src/donation_analytics.py
+++ src/donation_analytics.py
@@ -2,49 +2,14 @@ from __future__ import print_function
 
 from typing import List
 
+from Classes import Donation, Dictionary, Output
+
 import sys
 import re
 
 
 # 假设文件可以一次加载完
 # 修改方向：分割文件？分batch处理
-
-class Donation:
-	def __init__(self, CMTE_ID, NAME, ZIP_CODE, TRANSACTION_DT, TRANSACTION_AMT, OTHER_ID):
-		self.CMTE_ID = CMTE_ID
-		self.NAME = NAME
-		self.ZIP_CODE = ZIP_CODE
-		self.TRANSACTION_DT = TRANSACTION_DT
-		self.TRANSACTION_AMT = TRANSACTION_AMT
-		self.OTHER_ID = OTHER_ID
-
-	def isValid(self):
-		print("here")
-		if self.CMTE_ID is None or re.match('^[a-zA-Z0-9\s]*$', self.CMTE_ID) is None:
-			print(1)
-		if self.NAME is None or re.match('^[a-zA-Z0-9\s.,]*$', self.NAME) is None:
-			print(2)
-		if self.ZIP_CODE is None or len(self.ZIP_CODE) < 5 or re.match('^[0-9]*$', self.ZIP_CODE) is None:
-			print(3)
-		if self.TRANSACTION_DT is None or re.match('^(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(19|20)\d\d$', self.TRANSACTION_DT):
-			print(4)
-		if self.TRANSACTION_AMT is None:
-			print(5)
-		if self.OTHER_ID is not None:
-			print(6)
-	
-
-		if (
-			self.CMTE_ID is None or re.match('^[a-zA-Z0-9\s]*$', self.CMTE_ID) is None or
-			self.NAME is None or re.match('^[a-zA-Z0-9\s.,]*$', self.NAME) is None or
-			self.ZIP_CODE is None or len(self.ZIP_CODE) < 5 or re.match('^[0-9]*$', self.ZIP_CODE) is None or
-			self.TRANSACTION_DT is None or re.match('^(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(19|20)\d\d$', self.TRANSACTION_DT) or
-			self.TRANSACTION_AMT is None or 
-			self.OTHER_ID is not None
-			):
-
-			return False
-
 
 
 INPUT_FILE = open(sys.argv[1], 'r')
@@ -55,29 +20,48 @@ itcont_lines = INPUT_FILE.readlines()
 
 itcont_lines = list(map(lambda line : line.split('|'), itcont_lines))
 
+viewed = Dictionary()
+repeated = Dictionary()
+
 # for donation in itcont_lines:
 for record in [itcont_lines[1]]:
-	print(itcont_lines[1])
-	CMTE_ID = record[0]
-	NAME = record[8]
-	ZIP_CODE = record[11]
-	TRANSACTION_DT = record[13]
-	TRANSACTION_AMT = record[14]
-	OTHER_ID = record[15]
 
-	# if CMTE_ID is None or re.match('^[a-zA-Z0-9\s]*$', CMTE_ID) is None:
-	# 	continue
-	# if NAME is None or or re.match('^[a-zA-Z0-9\s.,]*$', NAME) is None:
-	# 	continue
-	# if ZIP_CODE is None or len(ZIP_CODE) < 5 or re.match('^[0-9]*$', ZIP_CODE) is None:
-	# 	continue
-	# if TRANSACTION_DT is None or re.match('^(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(19|20)\d\d$', TRANSACTION_DT):
-	# 	continue
-	# if TRANSACTION_AMT is None:
-	# 	continue
-	# if OTHER_ID is not None:
-	# 	continue
-	
+	CMTE_ID, NAME, ZIP_CODE = record[0], record[8], record[10][0:5]
+	TRANSACTION_DT, TRANSACTION_AMT, OTHER_ID = record[13], record[14], record[15]
+
 	donation = Donation(CMTE_ID, NAME, ZIP_CODE, TRANSACTION_DT, TRANSACTION_AMT, OTHER_ID)
-	if donation.isValid() is False:
-		print('TTTTTTTTT')
+
+	if donation.isValid():
+		don_id = donation.getID
+		
+		# 如果相同的人有过捐款记录，就是一个重复捐款的人
+		# 记录最新出现的一次捐款
+		if viewed.check(don_id):
+			viewed.pop(don_id)
+			repeated.add(donation.getRecipient(), donation.getZip(), donation.getYear(), donation.getAmt())
+			# more calculation
+			output = 
+			
+
+
+		if repeated.check(don_id):
+			repeated.add(don_id,donation.getRecipient(), donation.getZip(), donation.getYear())
+			# more calculation here
+			
+		
+		else:
+			viewed.add(don_id, donation.getYear())
+
+
+		print(viewed.get(don_id))
+		print(repeated.check(don_id))
+
+
+
+
+
+
+
+
+
+
